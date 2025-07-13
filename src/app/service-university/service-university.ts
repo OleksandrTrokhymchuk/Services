45053bab9f33a340cede020c14d6173ea77826a2
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-13 18:59:03 +0300

    feat: get neccesary fiels from json request

--- src/app/service-university/service-university.ts
+++ src/app/service-university/service-university.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core'
 import { SharedService } from '../shared.service'
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-service-university',
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './service-university.html',
   styleUrl: './service-university.scss'
 })
@@ -16,6 +17,12 @@ export class ServiceUniversity implements OnInit {
   protected apiData: any = null
   protected errorMessage: string | null = null
 
+  protected alpha_two_code: string | null = null
+  protected country: string | null = null
+  protected name: string | null = null
+  protected web_page: string | null = null
+  protected state_province: string | null = null
+
   fetchData(searchParams: string): void {
     const apiUrl = `http://universities.hipolabs.com/search?country=${searchParams}`
 
@@ -24,11 +31,18 @@ export class ServiceUniversity implements OnInit {
     this.http.get(apiUrl).subscribe({
       next: (response) => {
         this.apiData = response
-        console.log('response:', response)
+        console.log('response:', this.apiData)
+
+
+        this.alpha_two_code = this.apiData[0].alpha_two_code
+        this.country = this.apiData[0].country
+        this.name = this.apiData[0].name
+        this.web_page = this.apiData[0].web_pages[0]
+        this.state_province = this.apiData[0].state_province
       },
       error: (error) => {
         this.errorMessage = error.message
-        console.error('Помилка API-запиту:', error)
+        console.error('Error request to API:', error)
         this.apiData = null
       },
     })
