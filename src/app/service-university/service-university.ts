6d124a1aa45902a0b415989e0a9e6e17038b2eb0
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-13 17:42:12 +0300

    feat: add basic request to API

--- src/app/service-university/service-university.ts
+++ src/app/service-university/service-university.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core'
 import { SharedService } from '../shared.service'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-service-university',
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './service-university.html',
   styleUrl: './service-university.scss'
 })
 export class ServiceUniversity implements OnInit {
   receivedMessage = ''
 
-  constructor(private sharedService: SharedService) {}
+  constructor(private sharedService: SharedService, private http: HttpClient) {}
+
+  protected apiData: any = null
+  protected errorMessage: string | null = null
+
+  fetchData(searchParams: string): void {
+    const apiUrl = `http://universities.hipolabs.com/search?country=${searchParams}`
+
+    this.errorMessage = null
+
+    this.http.get(apiUrl).subscribe({
+      next: (response) => {
+        this.apiData = response
+        console.log('response:', response)
+      },
+      error: (error) => {
+        this.errorMessage = error.message
+        console.error('Помилка API-запиту:', error)
+        this.apiData = null
+      },
+    })
+  }
 
   ngOnInit() {
     this.sharedService.currentMessage.subscribe(message => {
       this.receivedMessage = message
+      this.fetchData(message)
     })
   }
 }
