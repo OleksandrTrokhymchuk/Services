f9a00a2708d72f21a2ed3238b697caca3a5ba65b
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-13 16:53:32 +0300

    feat: add catching input value

--- src/app/input/input.ts
+++ src/app/input/input.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input.html',
   styleUrl: './input.css'
 })
 export class Input {
+  protected inputValue: string = ""
+  protected displayedValue: string = ""
 
+  onSearchClick(): void {
+    this.displayedValue = this.inputValue
+    this.inputValue = ""
+  }
 }
